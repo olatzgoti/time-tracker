69d23c0b3f4ee238c6e77a293a23745f1271482b
olatzgoti <gotiolatz@gmail.com>
2025-09-12 19:57:39 +0200

    start pro

---
+++ time-tracker/src/app/dashboard-component/dashboard-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-component',
+  imports: [],
+  templateUrl: './dashboard-component.html',
+  styleUrl: './dashboard-component.css'
+})
+export class DashboardComponent {
+
+
+  
+}
---
+++ time-tracker/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tbaiooabpkbcqbvgjgel.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiYWlvb2FicGtiY3FidmdqZ2VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNjkwNzgsImV4cCI6MjA3Mjg0NTA3OH0.lFJ-Qn3Pj_KLiSlqHtQ7Dlvems4D62XbLJIlcf6enoQ',
+  }
